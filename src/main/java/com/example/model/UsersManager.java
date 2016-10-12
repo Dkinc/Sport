c17a61d4fdf1951c7dc83e9aeadaed0ddcd1ebeb
anngel91 <anngel91@gmail.com>
2016-10-12 16:36:07 +0300

    Password hashing

--- src/main/java/com/example/model/UsersManager.java
+++ src/main/java/com/example/model/UsersManager.java
@@ -1,62 +1,75 @@
 package com.example.model;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ConcurrentHashMap;
-
-import org.springframework.stereotype.Component;
 
 import com.example.model.db.UserDAO;
 
 public class UsersManager {
-	
-	private ConcurrentHashMap<String, User> registerredUsers;//username -> user
+
+	private ConcurrentHashMap<String, User> registerredUsers;// username -> user
 	private static UsersManager instance;
-	private UsersManager(){
+
+	private UsersManager() {
 		registerredUsers = new ConcurrentHashMap<String, User>();
-		for(User u : UserDAO.getInstance().getAllUsers()){
+		for (User u : UserDAO.getInstance().getAllUsers()) {
 			registerredUsers.put(u.getUsername(), u);
 		}
 	}
-	
-	public synchronized static UsersManager getInstance(){
-		if(instance == null){
+
+	public synchronized static UsersManager getInstance() {
+		if (instance == null) {
 			instance = new UsersManager();
 		}
 		return instance;
 	}
-	
-	public User getUser(String username){
+
+	public User getUser(String username) {
 		return registerredUsers.get(username);
 	}
-	
-	public boolean validLogin(String username, String password){
-		
-		if(!registerredUsers.containsKey(username)){
+
+	public boolean validLogin(String username, String password) {
+		String hashedPassword = passwordHash(password);
+		if (!registerredUsers.containsKey(username)) {
 			return false;
 		}
-		return registerredUsers.get(username).getPassword().equals(password);
+		return registerredUsers.get(username).getPassword().equals(hashedPassword);
 	}
-	
-	public boolean regUser(String username, String password, String email){
-		if(!registerredUsers.containsKey(username)){
-			if(username != null && username != "" && password.length() > 5 && password.length() < 45 && isValidEmailAddress(email)){			
-		User user = new User(username, password, email);
-		System.out.println("controller : " + user.getUsername() + "  " + user.getPassword() + "  "  + user.getEmail());
-		registerredUsers.put(username, user);
-		UserDAO.getInstance().saveUser(user);
-		return true;
-			}
-		}
-		else{
+
+	public void regUser(String username, String password, String email) {
+		String hashedPassword = passwordHash(password);
+		if (!registerredUsers.containsKey(username)) {
+
+			User user = new User(username, hashedPassword, email);
+			System.out
+					.println("controller : " + user.getUsername() + "  " + user.getPassword() + "  " + user.getEmail());
+			registerredUsers.put(username, user);
+			UserDAO.getInstance().saveUser(user);
+		} else {
 			System.out.println("Username already exists");
-			return false;
 		}
-		return false;
 	}
-	
-	private boolean isValidEmailAddress(String email) {
-		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-		java.util.regex.Matcher m = p.matcher(email);
-		return m.matches();
+
+	private static String passwordHash(String password) {
+		MessageDigest md;
+		StringBuffer sb = new StringBuffer();
+		try {
+			md = MessageDigest.getInstance("MD5");
+			md.update(password.getBytes());
+			byte[] b = md.digest();
+			for (byte b1 : b) {
+				String k = Integer.toHexString(b1 & 0xff).toString();
+				if (k.length() == 1) {
+					k = "0" + k;
+				}
+				sb.append(k);
+			}
+
+		} catch (NoSuchAlgorithmException e) {
+
+			e.printStackTrace();
+		}
+		return sb.toString();
 	}
 }
--- src/main/java/com/example/model/db/UserDAO.java
+++ src/main/java/com/example/model/db/UserDAO.java
@@ -1,5 +1,7 @@
 package com.example.model.db;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,27 +13,26 @@ import com.example.model.User;
 
 public class UserDAO {
 
-private static UserDAO instance;
-	
-	private UserDAO(){}
-	
-	public synchronized static UserDAO getInstance(){
-		if(instance == null){
+	private static UserDAO instance;
+
+	private UserDAO() {
+	}
+
+	public synchronized static UserDAO getInstance() {
+		if (instance == null) {
 			instance = new UserDAO();
 		}
 		return instance;
 	}
-	
-	public Set<User> getAllUsers(){
+
+	public Set<User> getAllUsers() {
 		Set<User> users = new HashSet<User>();
 		try {
 			Statement st = DBManager.getInstance().getConnection().createStatement();
 			ResultSet resultSet = st.executeQuery("SELECT username, password, email FROM users;");
-			while(resultSet.next()){
-				users.add(new User(	resultSet.getString("username"),
-									resultSet.getString("password"),
-									resultSet.getString("email")
-									));
+			while (resultSet.next()) {
+				users.add(new User(resultSet.getString("username"), resultSet.getString("password"),
+						resultSet.getString("email")));
 			}
 		} catch (SQLException e) {
 			System.out.println("SQL exception!!! Users not load!");
@@ -40,13 +41,15 @@ private static UserDAO instance;
 		System.out.println("Users loaded successfully");
 		return users;
 	}
-	
-	public void saveUser(User user){
+
+	public void saveUser(User user) {
 		try {
+			String hashedPassword = passwordHash(user.getPassword());
 			System.out.println(user.getEmail());
-			PreparedStatement st = DBManager.getInstance().getConnection().prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?);");
+			PreparedStatement st = DBManager.getInstance().getConnection()
+					.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?);");
 			st.setString(1, user.getUsername());
-			st.setString(2, user.getPassword());
+			st.setString(2, hashedPassword);
 			st.setString(3, user.getEmail());
 			st.executeUpdate();
 			System.out.println("User added successfully");
@@ -54,29 +57,53 @@ private static UserDAO instance;
 			System.out.println("SQL exception. Did not save the user!!!");
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public void writeNewPassword(String username, String password) {
 
 		try {
+			String hashedPassword = passwordHash(password);
 			Statement st = DBManager.getInstance().getConnection().createStatement();
-			st.executeUpdate("UPDATE users SET password = " + password +" WHERE username = " + username + " ;");
+			st.executeUpdate("UPDATE users SET password = " + hashedPassword + " WHERE username = " + username + " ;");
+		} catch (SQLException e) {
+			System.out.println("Problems with changing password!");
+			e.printStackTrace();
+		}
+	}
+
+	public void writeNewProfilePic(String username, String url) {
+
+		try {
+			Statement st = DBManager.getInstance().getConnection().createStatement();
+			st.executeUpdate(
+					"UPDATE user_profile SET profile_pic = " + url + " WHERE Users_username = " + username + " ;");
 		} catch (SQLException e) {
 			System.out.println("Problems with change password!");
 			e.printStackTrace();
 		}
 	}
 
-	public void writeNewProfilePic(String username, String url) {
-		
+	private static String passwordHash(String password) {
+		MessageDigest md;
+		StringBuffer sb = new StringBuffer();
 		try {
-			Statement st = DBManager.getInstance().getConnection().createStatement();
-			st.executeUpdate("UPDATE user_profile SET profile_pic = " + url +" WHERE Users_username = " + username + " ;");
-		} catch (SQLException e) {
-			System.out.println("Problems with change password!");
+			md = MessageDigest.getInstance("MD5");
+			md.update(password.getBytes());
+			byte[] b = md.digest();
+			for (byte b1 : b) {
+				String k = Integer.toHexString(b1 & 0xff).toString();
+				if (k.length() == 1) {
+					k = "0" + k;
+				}
+				sb.append(k);
+			}
+
+		} catch (NoSuchAlgorithmException e) {
+
 			e.printStackTrace();
 		}
+		return sb.toString();
 	}
-	
+
 }
